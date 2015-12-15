1fc6e700fd21a2ae67ce401e84839e76d11f4c9d
John <jpaxsontn@yahoo.com>
2015-12-15 08:32:07 -0600

    Added Lecture_55_Exercise_5

---
+++ Lecture_55_Exercise_4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_55_Exercise_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>();
+            Console.WriteLine("Enter a number or type \"Quit\" to exit");
+            var input = Console.ReadLine();
+            while (input.ToLower() != "quit")
+            {
+                int parsedInput;
+                if (!Int32.TryParse(input, out parsedInput))
+                {
+                    Console.WriteLine("Invalid input, try again");
+                }
+                else
+                {
+                    numbers.Add(parsedInput);
+                }
+
+                Console.WriteLine("Enter a number or type \"Quit\" to exit");
+                input = Console.ReadLine();
+            }
+
+            var uniqueNumbers = new List<int>();
+            foreach (var iNumber in numbers)
+            {
+                if (!uniqueNumbers.Contains(iNumber))
+                {
+                    uniqueNumbers.Add(iNumber);
+                }
+            }
+
+            Console.WriteLine("You entered the following unique numbers");
+            foreach (var iNumber in uniqueNumbers)
+            {
+                Console.WriteLine(iNumber);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lecture_55_Exercise_5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_55_Exercise_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a list of comma separated numbers (e.g 5, 1, 9, 2, 10)");
+            var numbers = new List<int>();
+            var input = Console.ReadLine();
+
+            while (numbers.Count < 5)
+            {
+                var nonParsedInputArray = input.Split(',');
+                foreach (var iInput in nonParsedInputArray)
+                {
+                    int validNumber;
+                    if (Int32.TryParse(iInput, out validNumber))
+                    {
+                        numbers.Add(validNumber);
+                    }
+                }
+
+                if (numbers.Count < 5)
+                {
+                    Console.WriteLine("Invalid List, please try again");
+                    input = Console.ReadLine();
+                    numbers.Clear();
+                }
+            }
+
+            Console.WriteLine("The three smallest numbers you entered are");
+            numbers.Sort();
+            for (var iCounter = 0; iCounter < 3; iCounter++)
+            {
+                Console.WriteLine(numbers[iCounter]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
