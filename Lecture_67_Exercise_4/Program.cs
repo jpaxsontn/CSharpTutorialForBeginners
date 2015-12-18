0ba995aa1c3f559aca3c6fe052062bd008ecbe64
jpaxsontn <jpaxsontn@yahoo.com>
2015-12-18 08:42:03 -0600

    Finished Lecture 67 Exercises

---
+++ Lecture_67_Exercise_4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_67_Exercise_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a few words seperated by a space");
+            var input = Console.ReadLine();
+            var words = input.ToLower().Split(' ').ToList();
+
+            Console.WriteLine("Pascal Variable = " + CreatePascalVariable(words));
+            Console.ReadKey();
+        }
+
+        private static StringBuilder CreatePascalVariable(List<string> words)
+        {
+            StringBuilder pascalVariable = new StringBuilder();
+            foreach(var iWord in words)
+            {
+                pascalVariable.Append(iWord.Substring(0, 1).ToUpper());
+                pascalVariable.Append(iWord.Substring(1));
+                
+            }
+
+            return pascalVariable;
+        }
+    }
+}
---
+++ Lecture_67_Exercise_5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_67_Exercise_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an English word");
+            var word = Console.ReadLine();
+            Console.WriteLine("Number of vowels = " + CountNumberOfVowels(word));
+
+            Console.ReadKey();
+        }
+
+        static int CountNumberOfVowels(string word)
+        {
+            char[] vowels = new char[] { 'a', 'e', 'i', 'o', 'u' };
+            int vowelCounter = 0;
+            for(var iCharacterIndex = 0; iCharacterIndex <= word.Length - 1; iCharacterIndex++)
+            {
+                foreach(var iVowel in vowels)
+                {
+                    if(word[iCharacterIndex].ToString().ToLower() == iVowel.ToString())
+                    {
+                        vowelCounter++;
+                        break;
+                    }
+                }
+            }
+
+            return vowelCounter;
+        }
+    }
+}
